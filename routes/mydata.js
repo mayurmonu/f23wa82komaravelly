2037509a60e56a6ce9958132d595af9a84aa7ebc
mayurmonu <143120384+mayurmonu@users.noreply.github.com>
2023-10-25 22:21:24 -0500

    Update mydata.js

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mayur Komaravelly' });
+  res.render('mydata', { title: 'Mayur Komaravelly' });
 });
 
 module.exports = router;
